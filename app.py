712b8419aae667609cb6e3c67d6ca2f9dd7eb806
malj <malj007@163.com>
2018-06-29 16:13:12 +0800

    F 2b

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return '<h1>Hello World!</h1>'
+
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, %s!</h1>' % name
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
