0f1dd54e89ccf4fcb2e9c3a30781d415a5282163
mali86007 <malj007@tom.com>
2018-06-29 21:56:36 +0800

    F 2c

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from flask import Flask
+from flask import request
+from flask import make_response
+from flask import redirect
+from flask import abort
 
 app = Flask(__name__)
-
+"""
+@app.route('/')
+def index():
+    user_agent = request.headers.get('User-Agent')
+    return '<p>Your browser is %s</p>' %user_agent
+"""
 @app.route('/')
 def hello_world():
     return '<h1>Hello World!</h1>'
@@ -9,6 +18,13 @@ def hello_world():
 @app.route('/user/<name>')
 def user(name):
     return '<h1>Hello, %s!</h1>' % name
-
+"""
+@app.route('/user/<id>')
+def get_user(id):
+    user = load_user(id)
+    if not user:
+        abort(400)
+    return '<h1>Hello, %s</h1>' % user.name
+"""
 if __name__ == '__main__':
     app.run(debug=True)
