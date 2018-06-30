44c904f34cc511173ce8552cff3b39fcbc7a6427
malj <malj007@163.com>
2018-06-30 12:58:50 +0800

    F 3a

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return '<h1>Hello World!</h1>'
+def index():
+    return render_template('index.html')
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, %s!</h1>' % name
+    return render_template('user.html',name=name)
 
 if __name__ == '__main__':
     app.run(debug=True)
